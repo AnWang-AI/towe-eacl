4fe06f7dc4d9bf07d18d880c35e453f7aefec90b
an_wang <an_wang@intsig.net>
2020-09-21 14:14:03 +0800

    modify

--- src/model/layers/ARGCN_dep_distance_conv.py
+++ src/model/layers/ARGCN_dep_distance_conv.py
@@ -86,6 +86,10 @@ class ARGCN_dep_distance_conv(MessagePassing):
 
         uniform(size, self.bias)
 
+        torch.nn.init.xavier_normal_(self.dep_emb_dim.weight)
+
+        torch.nn.init.xavier_normal_(self.distance_emb_dim.weight)
+
 
     def forward(self, x, edge_index, edge_type, edge_distance, edge_norm=None, size=None):
         """"""
