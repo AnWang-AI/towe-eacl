83e55aada863754a9e707a40efc2eca2f87d4f6d
an_wang <an_wang@intsig.net>
2020-09-28 17:30:50 +0800

    modify

--- run_train.sh
+++ run_train.sh
@@ -4,14 +4,6 @@ export PYTHONPATH=./
 
 
 ## 在服务器上运行
-CUDA_VISIBLE_DEVICES=1 python src/model/trainer.py \
---data_path /data/an_wang/towe-eacl/data/14res \
---save_model_name models/Tag-LSTM.ckpt \
---use_bert \
---model Target_BiLSTM_with_bert \
---loss FacalLoss \
---epoch 60 --train_batch_size 16 --val_batch_size 16 \
---cuda
 
 # LSTM
 CUDA_VISIBLE_DEVICES=1 python src/model/trainer.py \
@@ -24,12 +16,12 @@ CUDA_VISIBLE_DEVICES=1 python src/model/trainer.py \
 --cuda
 
 # bert LSTM
-CUDA_VISIBLE_DEVICES=7 python src/model/trainer.py \
+CUDA_VISIBLE_DEVICES=2 python src/model/trainer.py \
 --config_path ./src/model/config/conf_bert_lstm.ini \
---data_path ./data/16res \
+--data_path ./data/14lap \
 --epoch 40 --train_batch_size 32 \
 --eval_frequency 2 \
---save_model_name models/Model_Tag_BiLSTM_bert_16res.ckpt
+--save_model_name models/Model_Tag_BiLSTM_bert_14lap.ckpt
 
 # graph + LSTM
 CUDA_VISIBLE_DEVICES=7 python src/model/trainer.py \
@@ -46,57 +38,4 @@ CUDA_VISIBLE_DEVICES=0 python src/model/trainer.py \
 --epoch 40 --train_batch_size 32 \
 --eval_frequency 2 \
 --save_model_name models/Model_ExtractionNet_with_bert_with_graph.ckpt_3
-
-
-## 在windows里的ipython中运行
-# bert LSTM
-%run src/model/trainer.py \
---data_path ./data/14res \
---save_model_name models/Model_Target_BiLSTM_with_bert.ckpt \
---use_bert \
---model Target_BiLSTM_with_bert \
---loss FacalLoss \
---epoch 40 --train_batch_size 32 --val_batch_size 32 \
---eval_frequency 2 \
---cuda
-
-# LSTM
-%run src/model/trainer.py \
---data_path ./data/14res \
---save_model_name models/Model_Tag_BiLSTM.ckpt \
---model Tag_BiLSTM \
---loss CrossEntropy \
---epoch 20 --train_batch_size 32 --val_batch_size 32 \
---eval_frequency 1 \
---cuda
-
-%run src/model/trainer_crf.py \
---data_path ./data/14res \
---save_model_name models/Model_BiLSTM_CRF.ckpt \
---epoch 20 --train_batch_size 1 --val_batch_size 1 \
---eval_frequency 1 \
---cuda
-
-# graph
-%run src/model/trainer.py \
---data_path ./data/14res \
---save_model_name models/Model_ExtractionNet_with_graph.ckpt \
---build_graph \
---model ExtractionNet \
---loss CrossEntropy \
---epoch 40 --train_batch_size 32 --val_batch_size 32 \
---eval_frequency 2 \
---cuda
-
-# bert + graph
-%run src/model/trainer.py \
---data_path ./data/14res \
---save_model_name models/Model_ExtractionNet_with_bert_with_graph.ckpt \
---use_bert \
---build_graph \
---model Target_BiLSTM_with_bert \
---loss CrossEntropy \
---epoch 40 --train_batch_size 32 --val_batch_size 32 \
---eval_frequency 2 \
---cuda
 
