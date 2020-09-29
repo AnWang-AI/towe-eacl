d824c545e81d6cfd79e658bdcb10c0a9a547caf2
an_wang <an_wang@intsig.net>
2020-09-29 19:35:51 +0800

    modify

--- src/model/trainer.py
+++ src/model/trainer.py
@@ -44,7 +44,7 @@ model_config = config.config_dicts['model']
 
 
 def set_random_seed():
-    seed = 1024 #999
+    seed = 999
     np.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
