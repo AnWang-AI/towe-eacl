f890e0faa611e95cbc2cfb7c99806520fe6850b1
an_wang <an_wang@intsig.net>
2020-09-29 16:27:31 +0800

    modify

--- src/model/layers/ARGCN_dep_distance_conv.py
+++ src/model/layers/ARGCN_dep_distance_conv.py
@@ -186,7 +186,7 @@ class ARGCN_dep_distance_conv_ver2(MessagePassing):
         self.edge_feature_dim = edge_feature_dim
 
         self.negative_slope = 0.1
-        self.dropout = 0.4
+        self.dropout = 0.3
 
         self.neighbor_weight = Param(torch.Tensor(in_channels, out_channels))
 
