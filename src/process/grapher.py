2343a3efd704b1fe742275291360483c6dad7587
an_wang <an_wang@intsig.net>
2020-10-19 13:43:00 +0800

    modify

--- src/process/grapher.py
+++ src/process/grapher.py
@@ -7,7 +7,7 @@ from torch_geometric.data import Data
 class Grapher():
 
     def __init__(self, dep_map_path="./src/process/dep_map.json", tag_map_path="./src/process/tag_map.json"):
-        self.parser = spacy.load('./data/spacy_model/en_core_web_sm-2.2.5.tar.gz')
+        self.parser = spacy.load('./data/spacy_model/en_core_web_sm-2.2.5')
 
         with open(dep_map_path, 'r') as f:
             dep_map = json.load(f)
