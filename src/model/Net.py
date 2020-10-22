33d354547a336f26bfa85fa3ee58d1e6d715f153
an_wang <an_wang@intsig.net>
2020-10-22 14:38:28 +0800

    modify

--- src/model/Net.py
+++ src/model/Net.py
@@ -430,10 +430,10 @@ class ExtractionNet_mrc(torch.nn.Module):
         x = F.relu(x)
 
         x = torch.cat([x, question_rep], dim=-1)
-        print(batch.aspect.reshape(-1, 30))
+        # print(batch.aspect.reshape(-1, 30))
         # print(x.shape)
         # print(torch.ones(x.shape).cuda().shape)
-        x = self.self_att(x, torch.ones(x.shape[:2]).cuda())
+        x = self.self_att(x, batch.aspect.reshape(-1, 30)>0)
 
         x = F.relu(x)
         x = self.fin_lin(x)
