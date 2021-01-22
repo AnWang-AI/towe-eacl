b86c910a8dbeda78ddccd49ee773b578a0865d47
wang <wcwowwwww@gmail.com>
2021-01-22 20:11:09 +0900

    modify

--- src/model/ConfigParser.py
+++ src/model/ConfigParser.py
@@ -42,7 +42,7 @@ class Config:
             self.config_dicts['model']['train_batch_size'] = args.train_batch_size
         if args.epochs:
             self.config_dicts['model']['epochs'] = args.epochs
-        if args.num_mid_layers is None:
+        if args.num_mid_layers is not None:
             self.config_dicts['model']['num_mid_layers'] = args.num_mid_layers
 
     def print_config(self, output_file=None):
