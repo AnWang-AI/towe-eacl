6709abaa19573255bbfdc80552bd265279917143
wang <wcwowwwww@gmail.com>
2021-01-22 19:43:24 +0900

    modify

--- run.sh
+++ run.sh
@@ -2,6 +2,12 @@ export PYTHONPATH=./
 export model=w2v_gnn_lstm
 export dataset=14res
 
+if [ -d "./data/$dataset/processed" ]; then
+  rm -r ./data/$dataset/processed
+  echo "remove the dir ./data/$dataset/processed"
+fi
+
+
 CUDA_VISIBLE_DEVICES=0 python main.py \
 --config_path ./src/model/config/conf_$model.ini \
 --data_path ./data/$dataset \
