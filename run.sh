5948147eda6efb7a77c14ce9179d00ff88c5a258
wang <wcwowwwww@gmail.com>
2021-01-22 20:00:25 +0900

    modify

--- run.sh
+++ run.sh
@@ -2,10 +2,10 @@ export PYTHONPATH=./
 export model=w2v_gnn_lstm
 export dataset=14res
 
-if [ -d "./data/$dataset/processed" ]; then
-  rm -r ./data/$dataset/processed
-  echo "remove the dir ./data/$dataset/processed"
-fi
+#if [ -d "./data/$dataset/processed" ]; then
+#  rm -r ./data/$dataset/processed
+#  echo "remove the dir ./data/$dataset/processed"
+#fi
 
 
 CUDA_VISIBLE_DEVICES=0 python main.py \
