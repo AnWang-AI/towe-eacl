4a5d732bf4c47fece010624f26c2917eeb7a4c8a
wang <wcwowwwww@gmail.com>
2021-01-22 20:12:17 +0900

    modify

--- run.sh
+++ run.sh
@@ -7,11 +7,12 @@ export dataset=14res
 #  echo "remove the dir ./data/$dataset/processed"
 #fi
 
+export num_mid_layers=0
 
 CUDA_VISIBLE_DEVICES=0 python main.py \
 --config_path ./src/model/config/conf_$model.ini \
 --data_path ./data/$dataset \
 --epoch 50 --train_batch_size 16 \
---num_mid_layers 0 \
+--num_mid_layers $num_mid_layers \
 --eval_frequency 2 \
---save_model_name models/Model_ExtractionNet_$model_$dataset.ckpt
+--save_model_name models/Model_ExtractionNet_$model_mid_layer_num_$num_mid_layers_$dataset.ckpt
