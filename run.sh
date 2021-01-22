3727e3500f003f5797267fb20a31afaa8678ad2f
wang <wcwowwwww@gmail.com>
2021-01-22 20:06:42 +0900

    modify

--- run.sh
+++ run.sh
@@ -12,6 +12,6 @@ CUDA_VISIBLE_DEVICES=0 python main.py \
 --config_path ./src/model/config/conf_$model.ini \
 --data_path ./data/$dataset \
 --epoch 50 --train_batch_size 16 \
---num_mid_layers 0 \
+--num_mid_layers 4 \
 --eval_frequency 2 \
 --save_model_name models/Model_ExtractionNet_$model_$dataset.ckpt
